c37a8b302ed475568ee6ced3f75b6a7f4033f72d
root <root@instance-1.us-central1-a.c.euphoric-loop-286610.internal>
2020-08-19 02:55:49 +0000

    Updates

---
+++ precache-manifest.44f307598e5310e151cbadd53b3e653a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57ce301e4cf322c4df40e883a4493c11",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "a3f2a5d3fae39c221bbb",
+    "url": "/test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "176f56fe9d0e7bde14db",
+    "url": "/test/static/js/2.82de5b2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.82de5b2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3f2a5d3fae39c221bbb",
+    "url": "/test/static/js/main.c0062ac0.chunk.js"
+  },
+  {
+    "revision": "56f8028bc6bd4b2de1f2",
+    "url": "/test/static/js/runtime-main.40737025.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
